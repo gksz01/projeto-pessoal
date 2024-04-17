3a20841a692c6b3d020819af153cf169ffa4612d
Guilherme Cheutchuk <guilherme20051211@gmail.com>
2024-04-17 12:22:28 -0300

    Updating the Project

---
+++ src/schedule/agenda.py
@@ -0,0 +1,11 @@
+while True:
+    print("\nMenu da Agenda de Contatos:")
+    print("1. Adicionar Contato")
+    print("2. Ver lista de Contatos")
+    print("3. Atualizar Contatos")
+    print("4. Favoritar Contato")
+    print("5. Lista de Contatos Favoritos")
+    print("6. Deletar Contato da Agenda")
+    print("7. Sair")
+    break
+    
