bef3bf84ced55398cf35ba0a8b1fbdcfd37ed5fe
Guilherme Cheutchuk <guilherme20051211@gmail.com>
2024-04-18 12:09:39 -0300

    New function: update contact implemented

--- src/schedule/agenda.py
+++ src/schedule/agenda.py
@@ -13,8 +13,34 @@ def ver_contatos(contatos: list) -> List:
         nome = contato["nome"]
         telefone = contato["telefone"]
         email = contato["email"]
-        print(f"{indice}.[{favorito}] {nome} {telefone} {email}")   
+        print(f"{indice}.[{favorito}] Nome: {nome} || Telefone: {telefone} || Email: {email}")   
     return
+def update_nome(contatos: list, indice_contato: int, novo_nome_contato: str) -> Dict:
+    indice_contato_ajustado = int(indice_contato) - 1
+    contatos[indice_contato_ajustado]["nome"] = novo_nome_contato
+    print(f"Contato {indice_contato} atualizado para {novo_nome_contato}")
+    if dado_alterado == "nome":
+        return update_nome
+    else:
+        return "Você pode alterar apenas esses dados: Nome, Telefone e Email!"
+
+def update_telefone(contatos: list, indice_contato: int, novo_telefone_contato: any) -> Dict:
+    indice_contato_ajustado = int(indice_contato) - 1
+    contatos[indice_contato_ajustado]["telefone"] = novo_telefone_contato
+    print(f"Telefone do contato {indice_contato} atualizado para {novo_telefone_contato}")
+    if dado_alterado == "telefone":
+        return update_telefone
+    else:
+        return "Você pode alterar apenas esses dados: Nome, Telefone e Email!"
+
+def update_email(contatos: list, indice_contato: int, novo_email_contato: str) -> Dict:
+    indice_contato_ajustado = int(indice_contato) - 1
+    contatos[indice_contato_ajustado]["email"] = novo_email_contato
+    print(f"Email do contato {indice_contato} atualizado para {novo_email_contato}")
+    if dado_alterado == "email":
+        return update_email
+    else:
+        return "Você pode alterar apenas esses dados: Nome, Telefone e Email!"
 
 contatos = []
 
@@ -37,6 +63,21 @@ while True:
         adicionar_contato(contatos, nome, telefone, email)
     elif escolha == "2":
         ver_contatos(contatos)
+    elif escolha == "3":
+        ver_contatos(contatos)
+        indice_contato = input("Digite o numero do contato que deseja atualizar: ")
+        dado_alterado = input("Deseja alterar qual dado do contato? ")
+        if dado_alterado == "nome":
+            novo_nome = input("Digite o novo nome do contato: ")
+            update_nome(contatos, indice_contato, novo_nome)
+        elif dado_alterado == "telefone":
+            novo_telefone = input("Digite o novo numero do contato: ")
+            update_telefone(contatos, indice_contato, novo_telefone)
+        elif dado_alterado == "email":
+            novo_email = input("Digite o novo email do contato: ")
+            update_email(contatos, indice_contato, novo_email)
+        else:
+            print("Você pode alterar apenas esses dados: Nome, Telefone e Email!")
     elif escolha == "7":
         break
 
