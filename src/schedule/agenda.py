bb56dcf1b24ecab163d15715a3814a07d300bab1
Guilherme Cheutchuk <guilherme20051211@gmail.com>
2024-04-18 14:04:32 -0300

    Added New Function

--- src/schedule/agenda.py
+++ src/schedule/agenda.py
@@ -9,41 +9,70 @@ def adicionar_contato(contatos: list, nome: str, telefone: any, email: str) -> D
     return
 def ver_contatos(contatos: list) -> List:
     for indice, contato in enumerate(contatos, start=1):
-        favorito = "☆" if contato["favorito"] else " "
+        favorito = "♥" if contato["favorito"] else " "
         nome = contato["nome"]
         telefone = contato["telefone"]
         email = contato["email"]
         print(f"{indice}.[{favorito}] Nome: {nome} || Telefone: {telefone} || Email: {email}")   
     return
-def update_nome(contatos: list, indice_contato: int, novo_nome_contato: str) -> Dict:
-    indice_contato_ajustado = int(indice_contato) - 1
-    contatos[indice_contato_ajustado]["nome"] = novo_nome_contato
-    print(f"Contato {indice_contato} atualizado para {novo_nome_contato}")
+def update_nome(contatos: list, indice_contato: int, novo_nome_contato: str) -> str:
+    indice_ajustado = int(indice_contato) - 1
+    if indice_ajustado >= 0 and indice_ajustado < len(contatos):
+        contatos[indice_ajustado]["nome"] = novo_nome_contato
+        print(f"Contato {indice_contato} atualizado para {novo_nome_contato}")
+    else:
+        print("Indice da tarefa não corresonde com as existentes!")
+
     if dado_alterado == "nome":
         return update_nome
     else:
         return "Você pode alterar apenas esses dados: Nome, Telefone e Email!"
 
-def update_telefone(contatos: list, indice_contato: int, novo_telefone_contato: any) -> Dict:
-    indice_contato_ajustado = int(indice_contato) - 1
-    contatos[indice_contato_ajustado]["telefone"] = novo_telefone_contato
-    print(f"Telefone do contato {indice_contato} atualizado para {novo_telefone_contato}")
+def update_telefone(contatos: list, indice_contato: int, novo_telefone_contato: any) -> any:
+    indice_ajustado = int(indice_contato) - 1
+    if indice_ajustado >= 0 and indice_ajustado < len(contatos):
+        contatos[indice_ajustado]["telefone"] = novo_telefone_contato
+        print(f"Telefone do contato {indice_contato} atualizado para {novo_telefone_contato}")
+    else:
+        print("Indice da tarefa não corresonde com as existentes!")
+
     if dado_alterado == "telefone":
         return update_telefone
     else:
         return "Você pode alterar apenas esses dados: Nome, Telefone e Email!"
 
-def update_email(contatos: list, indice_contato: int, novo_email_contato: str) -> Dict:
-    indice_contato_ajustado = int(indice_contato) - 1
-    contatos[indice_contato_ajustado]["email"] = novo_email_contato
-    print(f"Email do contato {indice_contato} atualizado para {novo_email_contato}")
+def update_email(contatos: list, indice_contato: int, novo_email_contato: str) -> str:
+    indice_ajustado = int(indice_contato) - 1
+    if indice_ajustado >= 0 and indice_ajustado < len(contatos):
+        contatos[indice_ajustado]["email"] = novo_email_contato
+        print(f"Email do contato {indice_contato} atualizado para {novo_email_contato}")
+    else:
+        print("Indice da tarefa não corresonde com as existentes!")
+
     if dado_alterado == "email":
         return update_email
     else:
         return "Você pode alterar apenas esses dados: Nome, Telefone e Email!"
+    
+def favoritar_contato(contatos: list, indice_contato: int) -> str:
+    indice_ajustado = int(indice_contato) - 1
+    contatos[indice_ajustado]["favorito"] = True
+    print(f"Contato {indice_contato} adicionado aos Favoritos!")
+    if favoritar == "adicionar":
+        return favoritar_contato
+
+def desfavoritar_contato(contatos: list, indice_contato: int) -> str:
+    indice_ajustado = int(indice_contato) - 1
+    contatos[indice_ajustado]["favorito"] = False
+    print(f"Contato {indice_contato} removido dos Favoritos!")
+    if favoritar == "remover":
+        return desfavoritar_contato
+    
+def ver_favoritos(contatos: list):
+    return
 
 contatos = []
-
+contatos_favoritos = []
 while True:
     print("\nMenu de Agenda de Contatos:")
     print("1. Adicionar Contato")
@@ -66,7 +95,7 @@ while True:
     elif escolha == "3":
         ver_contatos(contatos)
         indice_contato = input("Digite o numero do contato que deseja atualizar: ")
-        dado_alterado = input("Deseja alterar qual dado do contato? ")
+        dado_alterado = input("Deseja alterar qual dado do contato? ").lower()
         if dado_alterado == "nome":
             novo_nome = input("Digite o novo nome do contato: ")
             update_nome(contatos, indice_contato, novo_nome)
@@ -78,6 +107,18 @@ while True:
             update_email(contatos, indice_contato, novo_email)
         else:
             print("Você pode alterar apenas esses dados: Nome, Telefone e Email!")
+    elif escolha == "4":
+        ver_contatos(contatos)
+        indice_contato = input("Digite qual contato deseja favoritar ou desfavoritar: ")
+        favoritar = input("Deseja adicionar ou remover este contato dos favoritos? ").lower()
+        if favoritar == "adicionar":
+            favoritar_contato(contatos, indice_contato)
+        elif favoritar == "remover":
+            desfavoritar_contato(contatos, indice_contato)
+        else:
+            print("Digite (adicionar) ou (remover) para prosseguir!")
+    elif escolha == "5":
+        ver_favoritos(contatos_favoritos)
     elif escolha == "7":
         break
 
